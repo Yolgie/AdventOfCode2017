d2dd6cb4c13fa00d9ea7afa442b268aec41425e8
Matthias Holzinger <matthias.holzinger@catalysts.cc>
2017-12-01 23:09:53 +0100

    generify for multiple iterations

--- day01.py
+++ day01.py
@@ -1,41 +1,58 @@
 import sys
 
-def get_input_filename(day, test):
-    return "day" + str(day).zfill(2) + ".input"
 
-def get_output_filename(day, test):
-    return "day" + str(day).zfill(2) + "." + str(test).zfill(2) + ".output"
+def get_output_filename(input_filename):
+    return input_filename.replace('input', 'output')
 
-def process(raw_input):
-    return calculate_sum_of_neighbors(parseInput(raw_input))
 
-def calculate_sum_of_neighbors(input_list):
-    sum = 0
-    for pos, current in enumerate(input_list):
-        if current == get_distant_digit(input_list, pos, int(len(input_list)/2)):
-            sum += current
-        last = current
-    return sum
-
-def calculate_sum_of_digits(input_list, distance):
-    sum = 0
-
-def get_distant_digit(intput_list, pos, distance):
-    otherPos = (pos+distance) % (len(intput_list))
-    return intput_list[otherPos]
-
-def parseInput(raw_input):
-    return [int(char) for char in list(raw_input)]
-
-if __name__ == "__main__":
+class Captcha:
     day = 1
     test = 2
 
-    with open(get_input_filename(day, test)) as input_file:
-        raw_input = input_file.readlines()[0]
+    def getDistance(self, input_lengt):
+        if self.day == 1 and self.test == 1:
+            return 1
+        if self.day == 1 and self.test == 2:
+            return int(input_lengt / 2)
 
-    result = process(raw_input)
-    print(result)
+    def get_input_filename(self):
+        return "day" + str(self.day).zfill(2) + ".input"
 
-    with open(get_output_filename(day, test), 'w') as output_file:
-        output_file.write(str(result))
+    def process(self, raw_input):
+        input_list = self.parseInput(raw_input)
+        distance = self.getDistance(len(input_list))
+        result = self.calculate_sum_of_digits(input_list, distance)
+        return result
+
+    def calculate_sum_of_digits(self, input_list, distance):
+        sum = 0
+        for pos, current in enumerate(input_list):
+            if current == self.get_distant_digit(input_list, pos, distance):
+                sum += current
+            last = current
+        return sum
+
+    @staticmethod
+    def get_distant_digit(intput_list, pos, distance):
+        otherPos = (pos + distance) % (len(intput_list))
+        return intput_list[otherPos]
+
+    def parseInput(self, raw_input):
+        return [int(char) for char in list(raw_input)]
+
+    def executeTestOnFile(self, input_filename):
+        with open(input_filename) as input_file:
+            raw_input = input_file.readlines()[0]
+
+        result = self.process(raw_input)
+        print(result)
+
+        with open(get_output_filename(input_filename), 'w') as output_file:
+            output_file.write(str(result))
+
+
+if __name__ == "__main__":
+    captcha = Captcha()
+    captcha.day = 1
+    captcha.test = 2
+    captcha.executeTestOnFile(captcha.get_input_filename())
--- test_day01.py
+++ test_day01.py
@@ -1,27 +1,41 @@
 import unittest
-from day01 import process
+from day01 import Captcha
 
-# class day0101Tests(unittest.TestCase):
-#     def test_sunshine(self):
-#         self.assertEqual(process("1122"), 3)
-#
-#     def test_all_same(self):
-#         self.assertEqual(process("1111"), 4)
-#
-#     def test_all_different(self):
-#         self.assertEqual(process("1234"), 0)
-#
-#     def test_wrap_around(self):
-#         self.assertEqual(process("91212129"), 9)
+class day0101Tests(unittest.TestCase):
+    captcha = Captcha()
 
-class day0102Tests(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.captcha.day = 1
+        self.captcha.test = 1
+
     def test_sunshine(self):
-        self.assertEqual(process("123425"), 4)
-        self.assertEqual(process("12131415"), 4)
+        self.assertEqual(self.captcha.process("1122"), 3)
 
     def test_all_same(self):
-        self.assertEqual(process("123123"), 12)
-        self.assertEqual(process("1212"), 6)
+        self.assertEqual(self.captcha.process("1111"), 4)
 
     def test_all_different(self):
-        self.assertEqual(process("1221"), 0)
+        self.assertEqual(self.captcha.process("1234"), 0)
+
+    def test_wrap_around(self):
+        self.assertEqual(self.captcha.process("91212129"), 9)
+
+class day0102Tests(unittest.TestCase):
+    captcha = Captcha()
+
+    def setUp(self):
+        super().setUp()
+        self.captcha.day = 1
+        self.captcha.test = 2
+
+    def test_sunshine(self):
+        self.assertEqual(self.captcha.process("123425"), 4)
+        self.assertEqual(self.captcha.process("12131415"), 4)
+
+    def test_all_same(self):
+        self.assertEqual(self.captcha.process("123123"), 12)
+        self.assertEqual(self.captcha.process("1212"), 6)
+
+    def test_all_different(self):
+        self.assertEqual(self.captcha.process("1221"), 0)
