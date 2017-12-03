55f82d86ab8b4b5ffd6e97622e0178c4fc68eb17
Matthias Holzinger <matthias.holzinger@catalysts.cc>
2017-12-03 13:23:17 +0100

    day 3

---
+++ day03.py
@@ -0,0 +1,103 @@
+import itertools
+
+import math
+
+
+class Grid:
+    grid = {}
+    last = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.set(0, 0, 1)
+        self.last = 0
+
+    def set(self, x, y, value):
+        self.grid[(x, y)] = value
+
+    def calculatePosition(self, x, y):
+        if (x,y) in self.grid:
+            return self.grid[(x,y)]
+        sumOfNeighbors = 0
+        neighbors = itertools.product([x - 1, x, x + 1], [y - 1, y, y + 1])
+        for coordinate in neighbors:
+            if coordinate in self.grid:
+                sumOfNeighbors += self.grid[coordinate]
+        self.set(x, y, sumOfNeighbors)
+        return sumOfNeighbors
+
+    def calculateCount(self, count):
+        return self.calculatePosition(*calculateSpiralPosition(count))
+
+    def calculateNext(self):
+        self.last = self.last + 1
+        return self.calculateCount(self.last)
+
+
+def calculateSpiralPosition(n):
+    k = math.ceil((math.sqrt(n) - 1) / 2)
+    t = 2 * k + 1
+    m = t * t
+    t = t - 1
+    if n >= m - t:
+        return (k - (m - n), -k)
+    else:
+        m = m - t
+    if n >= m - t:
+        return (-k, -k + (m - n))
+    else:
+        m = m - t
+    if n >= m - t:
+        return (-k + (m - n), k)
+    else:
+        return (k, k - (m - n - t))
+
+
+def calculateManhattanDistance(x, y):
+    return abs(x) + abs(y)
+
+
+class SpiralMemory:
+    day = 3
+    test = 2
+
+    def process(self, raw_input):
+        count = self.parseInput(raw_input)
+        if self.test == 1:
+            position = calculateSpiralPosition(count)
+            result = calculateManhattanDistance(*position)
+        if self.test == 2:
+            result = self.calculateFirstNumberLargerThan(count)
+        return result
+
+    def calculateFirstNumberLargerThan(self, target):
+        sum = 0
+        grid = Grid()
+        while sum <= target:
+            sum = grid.calculateNext()
+        return sum
+
+    def parseInput(self, raw_input):
+        for row in raw_input:
+            return int(row)
+
+    def executeTestOnFile(self, input_filename):
+        with open(input_filename) as input_file:
+            raw_input = input_file.readlines()
+
+        result = self.process(raw_input)
+        print(result)
+
+        with open(self.get_output_filename(), 'w') as output_file:
+            output_file.write(str(result))
+
+    def get_output_filename(self):
+        return "day" + str(self.day).zfill(2) + "." + str(self.test).zfill(2) + ".output"
+
+    def get_input_filename(self):
+        return "day" + str(self.day).zfill(2) + ".input"
+
+
+if __name__ == "__main__":
+    spiralMemory = SpiralMemory()
+    spiralMemory.executeTestOnFile(spiralMemory.get_input_filename())
---
+++ test_day03.py
@@ -0,0 +1,60 @@
+import unittest
+
+from day03 import SpiralMemory, Grid, calculateSpiralPosition
+
+
+class part01Tests(unittest.TestCase):
+    spiralMemory = SpiralMemory()
+
+    def setUp(self):
+        super().setUp()
+        self.spiralMemory.test = 1
+
+    def test_sample(self):
+        self.assertEqual(self.spiralMemory.process(["1"]), 0)
+        self.assertEqual(self.spiralMemory.process(["12"]), 3)
+        self.assertEqual(self.spiralMemory.process(["23"]), 2)
+        self.assertEqual(self.spiralMemory.process(["1024"]), 31)
+
+class part02(unittest.TestCase):
+    spiralMemory = SpiralMemory()
+
+    def setUp(self):
+        super().setUp()
+        self.spiralMemory.test = 2
+
+    def test_gird(self):
+        grid = Grid()
+        self.assertEqual(grid.calculateNext(), 1)
+        self.assertEqual(grid.calculateNext(), 2)
+        self.assertEqual(grid.calculateNext(), 4)
+        self.assertEqual(grid.calculateNext(), 5)
+        self.assertEqual(grid.calculateNext(), 10)
+        self.assertEqual(grid.calculateNext(), 11)
+        self.assertEqual(grid.calculateNext(), 23)
+        self.assertEqual(grid.calculateNext(), 25)
+        self.assertEqual(grid.calculateNext(), 26)
+        self.assertEqual(grid.calculateNext(), 54)
+        self.assertEqual(grid.calculateNext(), 57)
+
+    def test_calculate_spiral_position(self):
+        self.assertEqual(calculateSpiralPosition(1), (0,0))
+        self.assertEqual(calculateSpiralPosition(2), (1,0))
+        self.assertEqual(calculateSpiralPosition(3), (1,1))
+        self.assertEqual(calculateSpiralPosition(4), (0,1))
+        self.assertEqual(calculateSpiralPosition(5), (-1,1))
+        self.assertEqual(calculateSpiralPosition(6), (-1,0))
+
+    def test_first_greater(self):
+        self.assertEqual(self.spiralMemory.process(["0"]), 1)
+        self.assertEqual(self.spiralMemory.process(["1"]), 2)
+        self.assertEqual(self.spiralMemory.process(["2"]), 4)
+        self.assertEqual(self.spiralMemory.process(["3"]), 4)
+        self.assertEqual(self.spiralMemory.process(["4"]), 5)
+        self.assertEqual(self.spiralMemory.process(["5"]), 10)
+        self.assertEqual(self.spiralMemory.process(["6"]), 10)
+        self.assertEqual(self.spiralMemory.process(["7"]), 10)
+        self.assertEqual(self.spiralMemory.process(["8"]), 10)
+        self.assertEqual(self.spiralMemory.process(["10"]), 11)
+        self.assertEqual(self.spiralMemory.process(["400"]), 747)
+        self.assertEqual(self.spiralMemory.process(["800"]), 806)
