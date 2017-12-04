c98f07122fe7c34b1d981a469da2b278c6b4a041
Matthias Holzinger <matthias.holzinger@catalysts.cc>
2017-12-04 07:52:31 +0100

    day 4

---
+++ day04.py
@@ -0,0 +1,49 @@
+import itertools
+
+import math
+
+
+class Passphrases:
+    day = 4
+    test = 2
+
+    def process(self, raw_input):
+        passphrases = self.parseInput(raw_input)
+        validPasshrases = self.filterValidPassphrases(passphrases)
+        result = len(validPasshrases)
+        return result
+
+
+    def filterValidPassphrases(self, passphrases):
+        return [passphrase for passphrase in passphrases if self.isValidPassphrase(passphrase)]
+
+    def isValidPassphrase(self, passphrase):
+        if self.test == 1:
+            return len(passphrase) == len(set(passphrase))
+        if self.test == 2:
+            return len(passphrase) == len(set([''.join(sorted(password)) for password in passphrase]))
+
+
+    def parseInput(self, raw_input):
+        return [row.split() for row in raw_input]
+
+    def executeTestOnFile(self, input_filename):
+        with open(input_filename) as input_file:
+            raw_input = input_file.readlines()
+
+        result = self.process(raw_input)
+        print(result)
+
+        with open(self.get_output_filename(), 'w') as output_file:
+            output_file.write(str(result))
+
+    def get_output_filename(self):
+        return "day" + str(self.day).zfill(2) + "." + str(self.test).zfill(2) + ".output"
+
+    def get_input_filename(self):
+        return "day" + str(self.day).zfill(2) + ".input"
+
+
+if __name__ == "__main__":
+    spiralMemory = Passphrases()
+    spiralMemory.executeTestOnFile(spiralMemory.get_input_filename())
---
+++ test_day04.py
@@ -0,0 +1,16 @@
+import unittest
+
+from day04 import Passphrases
+
+
+class part01Tests(unittest.TestCase):
+    passphrases = Passphrases()
+
+    def setUp(self):
+        super().setUp()
+        self.passphrases.test = 1
+
+    def test_sample(self):
+        self.assertEqual(self.passphrases.process([["1","1","2"]]), 0)
+        self.assertEqual(self.passphrases.process([["1","1","2"],["1","3","2"],["1","4","2"]]), 2)
+
