6eb082e1a1d76f16151b5733acc0830c6e552d06
Matthias Holzinger <matthias.holzinger@catalysts.cc>
2017-12-07 02:30:38 +0100

    day 6

---
+++ day06.py
@@ -0,0 +1,68 @@
+import itertools
+
+
+class Memory:
+    currentState = []
+    history = list()
+
+    def __init__(self, memory) -> None:
+        super().__init__()
+        self.currentState = list(memory)
+        self.history = list()
+        hash_value = hash(tuple(self.currentState))
+        self.history.append(hash_value)
+
+    def execute(self):
+        start = self.currentState.index(max(self.currentState))
+        stop = start + self.currentState[start]
+        self.currentState[start] = 0
+        for pos in range(start + 1, stop + 1):
+            self.currentState[pos % len(self.currentState)] += 1
+        hash_value = hash(tuple(self.currentState))
+        if hash_value in self.history:
+            return len(self.history) - self.history.index(hash_value)
+        else:
+            self.history.append(hash_value)
+            return False
+
+
+class Memories:
+    day = 6
+    test = 2
+
+    def process(self, raw_input):
+        memory = Memory(self.parseInput(raw_input))
+        count = 0
+        while True:
+            count += 1
+            loopsize = memory.execute()
+            if loopsize is not False:
+                break
+        if self.test == 1:
+            return count
+        else:
+            return loopsize
+
+    def parseInput(self, raw_input):
+        return [[int(block) for block in row.split()] for row in raw_input][0]
+
+    def executeTestOnFile(self, input_filename):
+        with open(input_filename) as input_file:
+            raw_input = input_file.readlines()
+
+        result = self.process(raw_input)
+        print(result)
+
+        with open(self.get_output_filename(), 'w') as output_file:
+            output_file.write(str(result))
+
+    def get_output_filename(self):
+        return "day" + str(self.day).zfill(2) + "." + str(self.test).zfill(2) + ".output"
+
+    def get_input_filename(self):
+        return "day" + str(self.day).zfill(2) + ".input"
+
+
+if __name__ == "__main__":
+    exercise = Memories()
+    exercise.executeTestOnFile(exercise.get_input_filename())
---
+++ test_day06.py
@@ -0,0 +1,38 @@
+import unittest
+
+from day06 import Memory, Memories
+
+
+class part01Tests(unittest.TestCase):
+    memories = Memories()
+
+    def setUp(self):
+        super().setUp()
+        self.memories.test = 1
+
+    def test_sample(self):
+        self.assertEqual(self.memories.process(["0 2 7 0"]), 5)
+
+    def test_memory(self):
+        memory = Memory([0,2,7,0])
+        self.assertFalse(memory.execute())
+        self.assertEqual(memory.currentState, [2,4,1,2])
+        self.assertFalse(memory.execute())
+        self.assertEqual(memory.currentState, [3,1,2,3])
+        self.assertFalse(memory.execute())
+        self.assertEqual(memory.currentState, [0,2,3,4])
+        self.assertFalse(memory.execute())
+        self.assertEqual(memory.currentState, [1,3,4,1])
+        self.assertTrue(memory.execute())
+        self.assertEqual(memory.currentState, [2,4,1,2])
+
+class part02Tests(unittest.TestCase):
+    memories = Memories()
+
+    def setUp(self):
+        super().setUp()
+        self.memories.test = 2
+
+    def test_sample(self):
+        self.assertEqual(self.memories.process(["0 2 7 0"]), 4)
+
